67b090f418743e528949aa188ceecc89fcdd235c
Rapcampo <raphael.campospt@gmail.com>
2023-10-30 19:24:09 +0000

    the basis

---
+++ get_next_line.h
@@ -0,0 +1,16 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+//external libraries
+
+# include <unistd.h>
+# include <stdlib.h>
+
+// main function
+
+char	*get_next_line(int fd);
+
+// utils
+
+
+#endif
